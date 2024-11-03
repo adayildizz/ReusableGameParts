2b0505e86724bdde703173800a4ebfc2ff5d311d
adayildizz <adayildizz51@gmail.com>
2024-11-03 04:32:09 +0300

    player and box

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+@onready var collision_shape= $CollisionShape2D
+
 #@export var gravity : float = 0
 @export var jumping_speed : float
 @export var acceleration : float 
 @export var speed : float
+@export var push_force : float 
 
 @export var animations : AnimatedSprite2D
 @export var fsm : FSM
@@ -27,7 +30,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	get_input()
-	print(velocity)
 	velocity += get_gravity()*delta
 	coyote_time()
 	move()
@@ -41,17 +43,28 @@ func move():
 	velocity.x = lerp(velocity.x, input.x*speed, 0.1)
 	if velocity.x < 0:
 		animations.flip_h = true
+		animations.position.x = -7
 	else:
 		animations.flip_h = false
+		animations.position.x = 7
 	if input.y != 0:
 		if is_on_floor() or (!is_on_floor() and !coyote_timer.is_stopped()):
 			velocity += jumping_speed*Vector2.UP
 			
 	move_and_slide()
-	
+	apply_push_force()
 		
 
 func coyote_time():
 	if was_on_floor == true and is_on_floor()== false:
 		coyote_timer.start()
 	was_on_floor = is_on_floor()
+
+func apply_push_force():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var object = collision.get_collider()
+		if object is RigidBody2D:
+			object.apply_central_force(- collision.get_normal()*push_force)
+			#object.apply_central_force(Vector2.UP*get_gravity()*object.mass)
+	
